07449765b9342c97195639b308e067a9086e673d
DenysTheMenace <letitmaconstart@gmail.com>
2019-12-27 12:33:35 -0800

    added script.js

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ var firstName = 'Denys';
 console.log(firstName);
 
 var lastName = 'Polulyakh';
-var age = 34;
-console.log(lastName);
+var age = 28;
 
 var fullAge = true;
 console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Driver';
+console.log(job);
+
+/* Variable naming rules
+var _3years = 3;
+var johnMark = 'John and MArk';
+var if = 23;*/
