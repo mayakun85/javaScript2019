345f269a5a6cba8a7cde6ba0400a9020393b2e8a
DenysTheMenace <letitmaconstart@gmail.com>
2019-12-27 12:43:38 -0800

    just added new functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var firstName = 'Denys';
+/*var firstName = 'Denys';
 console.log(firstName);
 
 var lastName = 'Polulyakh';
@@ -13,7 +13,24 @@ console.log(job);
 job = 'Driver';
 console.log(job);
 
-/* Variable naming rules
+Variable naming rules
 var _3years = 3;
 var johnMark = 'John and MArk';
-var if = 23;*/
+var if = 23; */
+
+//Variable mutation and type coercion
+
+var firstName = 'John';
+var age = 28;
+// Type coercion
+console.log(firstName + ' ' + age);
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
